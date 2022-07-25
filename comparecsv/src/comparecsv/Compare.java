c774baef27d51b186e17c041146b34261c236446
kellieayy <kellie.awyong@gmail.com>
2022-07-25 22:20:41 +0800

    week10 submission

--- comparecsv/src/comparecsv/Compare.java
+++ comparecsv/src/comparecsv/Compare.java
@@ -8,62 +8,115 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Compare {
 
-	public static void main(String[] args) {
+	public static BufferedReader readCSVFile (String string) throws FileNotFoundException {
+		BufferedReader br = new BufferedReader(new FileReader(string));
+		return br;
+	}
+	
+	public static List<String> addToList(String line, BufferedReader file) {
+		List<String> file1 = new ArrayList<>();
+		try {
+			while ((line = file.readLine()) != null) {
+				file1.add(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return file1;
+	}
+	
+	public static ArrayList<String> computeDifferenceList(List<String> list1, List<String> list2) {
+		ArrayList<String> difference = new ArrayList<String>();
+		if (list1.size() > list2.size()) {
+			for (int i = 0; i < list2.size(); i++) {
+				if ((list1.get(i)).equals(list2.get(i))) {
+					continue;
+				}
+				else {
+					difference.add(list2.get(i));
+					difference.add(list1.get(i));
+				}
+			}
+			for (int j = list2.size(); j < list1.size(); j++) {
+				difference.add(list1.get(j));
+			}
+		} else if (list2.size() > list1.size()) {
+			for (int i = 0; i < list1.size(); i++) {
+				if ((list1.get(i)).equals(list2.get(i))) {
+					continue;
+				}
+				else {
+					difference.add(list2.get(i));
+					difference.add(list1.get(i));
+				}
+			}
+			for (int j = list1.size(); j < list2.size(); j++) {
+				difference.add(list2.get(j));
+			}
+		} else if (list1.size() == list2.size()) {
+			for (int i = 0; i < list2.size(); i++) {
+				if ((list1.get(i)).equals(list2.get(i))) {
+					continue;
+				}
+				else {
+					difference.add(list2.get(i));
+					difference.add(list1.get(i));
+				}
+			}
+		}
+		return difference;
+	}
+	
+	public static File writeToCSVFile(ArrayList<String> differenceList, String newFileName) {
+		File file = new File(newFileName);
+		try {
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write("Customer ID#, Account No,, Currency, Type, Balance");
+			bw.newLine();
+			for (int i = 0; i < differenceList.size(); i++) {
+				bw.write(differenceList.get(i));
+				bw.newLine();
+			}
+			bw.close();
+			fw.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return file;
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
 		String line = "";
 		String line2 = "";
 		List<String> lines = new ArrayList<>();
 		List<String> lines2 = new ArrayList<>();
 		ArrayList<String> difference = new ArrayList<String>();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("/Users/kellieayy/eclipse-workspace/comparecsv/src/comparecsv/sample_file_1.csv"));
-			BufferedReader br2 = new BufferedReader(new FileReader("/Users/kellieayy/eclipse-workspace/comparecsv/src/comparecsv/sample_file_2.csv"));
-			try {
-				while((line = br.readLine()) != null) {
-//					String[] values = line.split(",");
-//					System.out.println(values[0]);
-					lines.add(line);
-				}
-				while((line2 = br2.readLine()) != null) {
-					lines2.add(line2);
-				}
-			} 
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		for (int i=0; i<lines.size(); i++) {
-			if ((lines.get(i)).equals(lines2.get(i))) {
-				continue;
-			}
-			else {
-				difference.add(lines2.get(i));
-				difference.add(lines.get(i));
-			}
-		}
-		File file = new File("test.csv");
-		try {
-			FileWriter fw = new FileWriter(file);
-			BufferedWriter bw = new BufferedWriter(fw);
-			
-			bw.write("Customer ID#, Account No., Currency, Type, Balance");
-			bw.newLine();
-			for (int j=0; j<difference.size(); j++) {
-				bw.write(difference.get(j));
-				bw.newLine();
-			}
-			bw.close();
-			fw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+		BufferedReader br;
+		BufferedReader br2;
+//		try {
+//			BufferedReader br = new BufferedReader(new FileReader("/Users/kellieayy/eclipse-workspace/comparecsv/src/comparecsv/sample_file_1.csv"));
+//			BufferedReader br2 = new BufferedReader(new FileReader("/Users/kellieayy/eclipse-workspace/comparecsv/src/comparecsv/sample_file_3.csv"));
+//			
+//			lines = addToList(line, br);
+//			lines2 = addToList(line2, br2);
+//		}
+//		catch(FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+		br = readCSVFile("/Users/kellieayy/eclipse-workspace/comparecsv/src/comparecsv/sample_file_1.csv");
+		br2 = readCSVFile("/Users/kellieayy/eclipse-workspace/comparecsv/src/comparecsv/sample_file_3.csv");
+		lines = addToList(line, br);
+		lines2 = addToList(line2, br2);
+		difference = computeDifferenceList(lines, lines2);
+		writeToCSVFile(difference, "difference1_3.csv");
+		
 	}
 
 }
